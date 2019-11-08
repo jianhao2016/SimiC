bb31e7839e9ee1cc5d58dc7fc56f74e6cf4b7de3
jianhao <jianhao2@illinois.edu>
2019-11-08 11:58:01 -0600

    jianhao: change imputation, done. [TODO] unfinished loading

--- code/simiclasso/magic_smooth.py
+++ code/simiclasso/magic_smooth.py
@@ -16,7 +16,7 @@ import ipdb
 import scprep
 import os
 
-def magic_op_on_df(p2df, p2fc, p2save_df):
+def magic_op_on_df(p2df, p2save_df, df_with_label = False):
     '''
     read a pandas dataframe, performs magic on selected feature columns,
     Return an imputed dataframe.
@@ -32,13 +32,17 @@ def magic_op_on_df(p2df, p2fc, p2save_df):
     # print(type(tmp))
     # print(tmp)
     #tmp = scprep.filter.remove_empty_genes(tmp)
-    if os.path.isfile(p2fc):
-        fc_path = p2fc
+    # if os.path.isfile(p2fc):
+    #     fc_path = p2fc
+    # else:
+    #     raise ValueError('{} is not a valid feature columns'.format(p2fc))
+    # # fc_path = os.path.join(root, 'diff_gene_list', 'df_feature_column_mouse')
+    # with open(fc_path, 'rb') as f:
+    #     feat_col = pickle.load(f)
+    if df_with_label:
+        feat_col = tmp.columns[:-1]
     else:
-        raise ValueError('{} is not a valid feature columns'.format(p2fc))
-    # fc_path = os.path.join(root, 'diff_gene_list', 'df_feature_column_mouse')
-    with open(fc_path, 'rb') as f:
-        feat_col = pickle.load(f)
+        feat_col = tmp.columns
     X = tmp[feat_col]
     # in case there are na values in dataframe, fill it with zero.
     X.fillna(0, inplace = True)
@@ -47,7 +51,8 @@ def magic_op_on_df(p2df, p2fc, p2save_df):
     X_magic = magic_op.fit_transform(X)
     
     
-    X_magic['label'] = tmp['label']
+    if df_with_label:
+        X_magic['label'] = tmp['label']
     print('shape of X_magic:', X_magic.shape)
     # X_magic.to_pickle(os.path.join(root, 'pandas_dataframe_mouse'))
     print('saving imputed dataframe to: ', p2save_df)
