dd0f67063a31dedc92285a7c28d2703de2b333c9
jianhao <jianhao2@illinois.edu>
2020-10-20 01:07:34 -0500

    update to version 0.3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
         name = 'simiclasso',
-        version = '0.3.1',
+        version = '0.3.2',
         author = 'Jianhao Peng',
         author_email = 'jianhao2@illinois.edu',
         description = 'initial package for simiclasso',
